6790416c64aa642fbb41e2a7bdb98337e9a9aa89
War <warhawo@gmail.com>
2023-04-18 21:44:51 +0200

    execice complited + validated

---
+++ Basic_Operators/exercice.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+COST_PINEAPPLE=50
+
+COST_BANANA=4
+
+COST_WATERMELON=23
+
+COST_BASKET=1
+
+TOTAL=$(($COST_PINEAPPLE + 2 * $COST_BANANA + 3 * $COST_WATERMELON + $COST_BASKET))
+
+echo "Total Cost is $TOTAL"
+
+#In this exercise, you will need to calculate to total cost (variable TOTAL) of a fruit basket, which contains 1 pineapple, 2 bananas and 3 watermelons.
